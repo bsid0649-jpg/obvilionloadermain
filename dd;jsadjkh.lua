8bd2fa3195a5ff4accbc7f902d16260ec685b6ec
bsid0649-jpg <bsid0649@gmail.com>
2026-08-19 19:17:20 +0500

    Update dd;jsadjkh.lua

--- dd;jsadjkh.lua
+++ dd;jsadjkh.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/bsid0649-jpg/obvilionloadermain/refs/heads/main/dd;jsadjkh.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bsid0649-jpg/obvilionloadermain/refs/heads/main/sddasd.lua"))()
