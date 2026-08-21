22e505227d89b956d54d3e5bc40ad23fcfd7e70b
bsid0649-jpg <bsid0649@gmail.com>
2026-08-21 18:42:34 +0500

    Update whwpuhuhjh.lua

--- whwpuhuhjh.lua
+++ whwpuhuhjh.lua
@@ -1,2 +1,2 @@
-getgenv().SCRIPT_KEY = "8bae739f-af07-484d-a698-ccd0110e9b5b";
+getgenv().SCRIPT_KEY = "abb50201-cd97-4587-b9d9-a60bc7f4ea78";
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4395e7af34a9c97fae0b176dc76a8b97035572061021d5e65fdb43398110ee74/download"))()
